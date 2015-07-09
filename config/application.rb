35c297e64aaa5630ca296c4185788e9928ed7519
JaredCowan <jaredlc@outlook.com>
2015-07-08 17:27:18 -0700

    added favicon

--- config/application.rb
+++ config/application.rb
@@ -10,22 +10,17 @@ require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module PalmLandscaping
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'America/Los_Angeles'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    # Disable I18n locale deprecation warning caused by newrelic gem
+    # http://stackoverflow.com/questions/20361428/rails-i18n-validation-deprecation-warning
+    I18n.enforce_available_locales = true
+
+    config.i18n.default_locale = :en
   end
 end
