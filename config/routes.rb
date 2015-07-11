30a608ae60f1380ef1987a3f523be5191070f516
JaredCowan <jaredlc@outlook.com>
2015-07-11 01:27:30 -0700

    added rough jobs layout

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show], path: "/users"
 
-  resources :static_pages, only: :index, path: "/:action", as: ":action"
+  resources :jobs, only: :index
+
+  resources :static_pages, only: :index, path: "/:action", as: :action
   get "/dashboard", to: "static_pages#dashboard", as: :home
 
   root 'static_pages#login'
