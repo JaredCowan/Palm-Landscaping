4b28a04f0f47c3a9c6dd0a9e2f06b407feb1ad2d
JaredCowan <jaredlc@outlook.com>
2015-07-11 16:45:10 -0700

    added job filter

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,8 +5,10 @@
 //= require vendor/scroll-to
 //= require vendor/nicescroll
 //= require vendor/moris-charts
+//= require vendor/list
 //= require js-routes
 //= require no-autocomplete-rails
 //= require global
 //= require components/sidebars
 //= require views/dashboard
+//= require views/jobs
---
+++ app/assets/javascripts/views/jobs.js
@@ -0,0 +1,29 @@
+!(function($) {
+  $(document).ready(function() {
+    var $body = $("body");
+
+    if ($body.hasClass("controller-jobs")) {
+
+      var searchOptions = {
+        valueNames: [ 'search__job__name', 'search__job__date' ]
+      };
+
+      var jobsList = new List('search__jobs', searchOptions);
+      var $search  = $(".search");
+
+      $search.on("keyup", function() {
+        var $searchGroup = $(".search-group");
+        var $searchLabel = $(".search-group label");
+        var results      = jobsList.matchingItems;
+
+        if ($search.val().length > 0 && results.length === 0) {
+          $searchGroup.addClass("has-error");
+          $searchLabel.show();
+        } else if ($search.val().length === 0) {
+          $searchGroup.removeClass("has-error");
+          $searchLabel.hide();
+        }
+      });
+    }
+  });
+})(jQuery);
