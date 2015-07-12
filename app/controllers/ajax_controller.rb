c884a96ac483531dc390858da0b414af7ec620f0
JaredCowan <jaredlc@outlook.com>
2015-07-12 02:42:23 -0700

    ajaxed job views

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -1,6 +1,7 @@
 class AjaxController < ApplicationController
   def load
-    built_path = params[:soft].gsub("_", "/")
+    built_path = params[:soft].gsub("_", "/") || "ajax/load"
+    hard_path  = params[:hard].gsub("_", "/").gsub("-", "_")
     respond_to do |format|
       format.js { render built_path, layout: false }
     end
